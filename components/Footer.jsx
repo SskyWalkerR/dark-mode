21323f63d5afadb092d3ee31f2778b870af0f980
SskyWalkerR <sammanualvarghese@gmail.com>
2022-01-07 14:10:06 +0530

    edit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="flex justify-center items-center h-16 bg-black text-white">
-            <p>Copyright © 2021 EGG All rights reserved.</p>
+            <p>Copyright © 2021 All rights reserved.</p>
         </div>
     );
 };
